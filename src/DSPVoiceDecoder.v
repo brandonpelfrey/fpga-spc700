3966c6402fd1df876e6891448f0f95449de36299
Brandon Pelfrey <brandonpelfrey@gmail.com>
2021-09-23 09:35:57 -0700

    Update DSPVoiceDecoder.v

--- src/DSPVoiceDecoder.v
+++ src/DSPVoiceDecoder.v
@@ -1,3 +1,29 @@
+// The DSPVoiceDecoder represents the logic to load compressed BRR sample data from memory, decode
+// that data, and output an audio sample value interpolated from the most recent samples. 
+
+// The pitch for a voice, provided by the DSP, is in the range [0,2^14). Playing back a sample at
+// it's original speed would imply a pitch of 4096, which means one nibble of BRR data (which
+// represents one output sample value) would be consumed every 1/32khz seconds. Pitches >4096 imply
+// playback at a higher audible pitch, and necessitates actually reading sample data more quickly.
+// The maximum pitch supported on the DSP is +2 octaves, which is quadruple the normal speed.
+// Pitch values can also be much lower, at the cost of distortion.
+
+// This is conceptualized in logic below by imagining each sample is separated by 4096 steps, and
+// a "cursor" which points somewhere between the the second most-recent and most-recently decoded
+// sample values. When the cursor extends beyond 4096, the two samples being interpolated for out-
+// put are advanced. 
+
+// For every output sample that the voice decoder needs to produce, there are many clock cycles. 
+// The DSP itself schedules which cycles a voice decoder should step forward so that only one 
+// decoder is ever trying to read from memory at a time. The internal state machine of the module
+// insures in a fixed number of cycles representing the worst case requirements, a header byte and
+// two BRR sample bytes may be read to fill the buffer. When the ring buffers for sample data are
+// sufficiently full, the decoder advances to a processing state to ensure that the BRR data is
+// decompressed and the cursor is advanced to the correct position.
+
+// Output from the voice decoder is currently a linear interpolation of the most-recent samples, 
+// however, the real hardware does a gaussian filtering of the last few samples.
+
 module DSPVoiceDecoder (
   clock,
   reset,
@@ -42,6 +68,7 @@ output reg [15:0] cursor;
 reg [2:0]  cursor_i;
 reg [2:0]  unused_samples;
 
+// Ring buffer of decompressed samples
 reg signed [15:0] read_buffer   [7:0];
 reg        [1:0]  filter_buffer [7:0];
 
@@ -49,11 +76,14 @@ reg [2:0] read_buffer_index;
 reg [3:0] block_index;
 reg signed [15:0] previous_samples [3:0];
 
-// Headers for current bytes being decoded vs next
+// Headers for current bytes being decoded
 reg [7:0] header;
 wire header_end   = header[0];
 wire header_loop  = header[1];
 
+// When the end flag is detected for a block of data, the header
+// expresses whether or not we should loop back to the beginning
+// of the sample, or stop playback.
 wire final_block_do_end  = header_end & (!header_loop);
 wire final_block_do_loop = header_end & header_loop;
 
@@ -64,6 +94,7 @@ parameter STATE_INIT = 0,
           STATE_OUTPUT_AND_WAIT = 4,
           STATE_END = 5;
 
+// Current and next read ring buffer slots
 wire [2:0] rbi0 = read_buffer_index;
 wire [2:0] rbi1 = (read_buffer_index + 1) & 7;
 
@@ -102,17 +133,19 @@ always @(posedge clock) begin
 
       ///////////////////////////////////////////////////////////////////////////////////
       STATE_INIT: begin
+        // We wait in the init state until the DSP decides via schedule to start this
+        // voice so it may produce a new sample value
         if(advance_trigger) begin
           ram_address      <= start_address;
           ram_read_request <= 1;
           state            <= STATE_READ_HEADER;
           reached_end      <= 0;
-          //$display("INIT %m cursor %d", cursor);
         end
       end
 
       ///////////////////////////////////////////////////////////////////////////////////
       STATE_READ_HEADER: begin
+        // RAM data lines currently pointing at the header for a BRR block
         header           <= ram_data;
         state            <= STATE_READ_DATA;
         ram_address      <= ram_address + 1;
@@ -122,9 +155,15 @@ always @(posedge clock) begin
 
       ///////////////////////////////////////////////////////////////////////////////////
       STATE_READ_DATA: begin
+        // BRR sample data is expressed as one nibble per sample, sign extended, and
+        // zero-padded by an amount given in the header. We always read two input nibbles
+        // at a time, which produces two bytes in our ring buffer.
         read_buffer[rbi0]   <= { {12{ram_data[7]}}, ram_data[7:4] } << header[7:4];
         read_buffer[rbi1]   <= { {12{ram_data[3]}}, ram_data[3:0] } << header[7:4];
 
+        // There are various filtering modes for incoming data. If the data is ADPCM 
+        // encoded, then the later processing of this sample data will need to know which
+        // filter was supposed to be applied for this block.
         filter_buffer[rbi0] <= header[3:2];
         filter_buffer[rbi1] <= header[3:2];
 
@@ -133,15 +172,19 @@ always @(posedge clock) begin
         block_index <= block_index + 1;
 
         if (unused_samples >= 2) begin
-          //$display("DATA: %m cursor %d", cursor);
+          // If there are at least 2 unused samples, then we have read everything that we
+          // need. If the cursor has overflowed, then we need to perform sample processing.
+          // Otherwise, we're done and output is ready.
           state            <= (cursor >= 4096) ? STATE_PROCESS_SAMPLE : STATE_OUTPUT_AND_WAIT;
           ram_read_request <= 0;
         end else begin
+          // BRR data comes in blocks of 8 samples. Check whether we are looping or not.
           if (block_index == 7) begin
             state            <= final_block_do_end  ? STATE_END : STATE_READ_HEADER;
             ram_address      <= final_block_do_loop ? loop_address : (ram_address + 1);
             ram_read_request <= !final_block_do_end;
           end else begin
+            // We're in the middle of a set of BRR data blocks
             state       <= STATE_READ_DATA;
             ram_address <= ram_address + 1;
             ram_read_request <= 1;
@@ -152,6 +195,8 @@ always @(posedge clock) begin
       ///////////////////////////////////////////////////////////////////////////////////
       STATE_PROCESS_SAMPLE: begin
         // We can only be in this state if cursor >= 4096 in the cursor.
+        // Other combinational logic in the module 'filter_out' computes the appropriate
+        // filtered sample value depending on the state of the header data (Raw, ADPCM, etc.)
         previous_samples[3] <= previous_samples[2];
         previous_samples[2] <= previous_samples[1];
         previous_samples[1] <= previous_samples[0];
